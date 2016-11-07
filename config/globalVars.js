13e6bcd79f0795344cbd14325df7e6abb7707d3d
Dilpreet Singh <preet19@outlook.com>
2016-11-07 15:49:03 -0500

    first commit

---
+++ config/globalVars.js
@@ -0,0 +1,11 @@
+/**
+ * Created by RFreeman on 10/7/2016.
+ */
+
+// object holding global variables
+module.exports = {
+  //db: 'mongodb://localhost/comp2068-fri',
+
+  db: 'mongodb://leonight:karan100@ds031865.mlab.com:31865/dilpreet',
+  secret: 'Some random string used to salt 123'
+};
